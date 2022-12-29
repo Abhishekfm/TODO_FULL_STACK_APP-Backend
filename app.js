4be6bf5867b3236bbd85c6ece50658ac98dfa540
Abhishekfm <62643405+Abhishekfm@users.noreply.github.com>
2022-12-29 12:34:03 +0530

    methods

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ const cors = require('cors');
 const corsOptions ={
     origin:'https://todo-full-stack-app-frontend.vercel.app', 
     credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    optionSuccessStatus:200,
+    methods:['GET', 'PUT', 'POST']
 }
 app.use(cors(corsOptions));
 
