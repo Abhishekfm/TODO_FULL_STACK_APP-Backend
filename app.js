bd2d3cb44c80c02c6b256aafa7a1295017bf9d45
Abhishekfm <62643405+Abhishekfm@users.noreply.github.com>
2022-12-29 11:53:42 +0530

    origin

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 // }))
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://todo-full-stack-app-frontend.vercel.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
